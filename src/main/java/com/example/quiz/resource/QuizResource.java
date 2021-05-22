c0b151510834d3f8c429a48510740126d8290d9b
Srikanth <srikanthketepalli555@gmail.com>
2021-05-22 14:10:40 +0530

    move java to 1.8

--- src/main/java/com/example/quiz/resource/QuizResource.java
+++ src/main/java/com/example/quiz/resource/QuizResource.java
@@ -1,20 +1,26 @@
 package com.example.quiz.resource;
 
 import com.example.quiz.dto.QuizCategoryListResponse;
-import com.example.quiz.service.QuizService;
+import com.example.quiz.service.QuizServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @RestController
 public class QuizResource {
 
     @Autowired
-    private QuizService quizService;
+    private QuizServiceImpl quizServiceImpl;
+
+    Logger logger = Logger.getLogger(String.valueOf(QuizResource.class));
 
     @GetMapping("/coding/exercise/quiz")
     public ResponseEntity<QuizCategoryListResponse> getQuiz() {
-        return ResponseEntity.ok(quizService.getQuizzes());
+        logger.log(Level.INFO, "Received request to get quizzes");
+        return ResponseEntity.ok(quizServiceImpl.getQuizzes());
     }
 }
